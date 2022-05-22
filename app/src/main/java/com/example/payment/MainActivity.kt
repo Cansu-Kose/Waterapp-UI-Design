024b8dc25482737ba93ec8c0bd3e4f2e408f06fd
Cansu Kose <cansuu23977@gmail.com>
2022-05-23 00:52:19 +0300

    menu eklendi

--- app/src/main/java/com/example/payment/MainActivity.kt
+++ app/src/main/java/com/example/payment/MainActivity.kt
@@ -3,7 +3,10 @@ package com.example.payment
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.databinding.DataBindingUtil
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupWithNavController
 import com.example.payment.databinding.ActivityMainBinding
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
@@ -12,6 +15,10 @@ class MainActivity : AppCompatActivity() {
         binding=DataBindingUtil.setContentView(this,R.layout.activity_main)
         //setContentView(R.layout.activity_main)
         init()
+        val navHostFragment=supportFragmentManager.findFragmentById(R.id.fragmentContainerView2) as NavHostFragment
+        val navController=navHostFragment.navController
+        binding.bottomNavigation.setupWithNavController(navController)
+
     }
     private fun init(){
 
