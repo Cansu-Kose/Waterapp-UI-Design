f50960b2345d346fc196275df46775cc217b6147
Cansu Kose <cansuu23977@gmail.com>
2022-05-23 01:19:21 +0300

    hata duzeltildi

--- app/src/main/java/com/example/payment/MainActivity.kt
+++ app/src/main/java/com/example/payment/MainActivity.kt
@@ -1,7 +1,10 @@
 package com.example.payment
 
+import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.AttributeSet
+import android.view.View
 import androidx.databinding.DataBindingUtil
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.setupWithNavController
